656a48e1cb007518d2104590c885f3d2b2ba11e8
Aaron Greider <40152187+aarongreider@users.noreply.github.com>
2024-03-08 15:51:01 -0500

    Updating pos on custom node

--- src/TextUpdaterNode.tsx
+++ src/TextUpdaterNode.tsx
@@ -1,28 +1,40 @@
 import { useCallback } from 'react';
-import { Handle, Position } from 'reactflow';
+import { Handle, Position, NodeProps, useNodes, useReactFlow } from 'reactflow';
 
 const handleStyle = { left: 10 };
 
-function TextUpdaterNode({ data, isConnectable }: any) {
+function TextUpdaterNode( props: NodeProps) {
+  const nodes = useNodes();
+  const reactFlowInstance = useReactFlow();
+
+
   const onChange = useCallback((evt: any) => {
     console.log(evt.target.value);
+    console.log(props.xPos)
+    console.log(props.id)
+    console.log(nodes)
+    console.log(reactFlowInstance)
+    console.log(reactFlowInstance.getNode('1'))
   }, []);
 
   return (
     <div className="text-updater-node">
-      <Handle type="target" position={Position.Top} isConnectable={isConnectable} />
+      <Handle type="target" position={Position.Top} isConnectable={props.isConnectable} />
       <div>
+     
         <label htmlFor="text">Text:</label>
         <input id="text" name="text" onChange={onChange} className="nodrag" />
       </div>
+      <p>There are currently {nodes.length} nodes!</p>
+      <p>x: {reactFlowInstance.getNode('1')?.position.x}  y: {reactFlowInstance.getNode('1')?.position.y}</p>
       <Handle
         type="source"
         position={Position.Bottom}
         id="a"
         style={handleStyle}
-        isConnectable={isConnectable}
+        isConnectable={props.isConnectable}
       />
-      <Handle type="source" position={Position.Bottom} id="b" isConnectable={isConnectable} />
+      <Handle type="source" position={Position.Bottom} id="b" isConnectable={props.isConnectable} />
     </div>
   );
 }
