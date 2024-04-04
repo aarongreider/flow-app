54f057227dc9aed607f3a036ef481c6716828632
Aaron Greider <40152187+aarongreider@users.noreply.github.com>
2024-04-04 10:04:58 -0400

    change redirect path

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       domain="dev-k7eu453201ptp0k4.us.auth0.com"
       clientId="4k6jPszthGusV3fd3cFwd7aML9g3h1cc"
       authorizationParams={{
-        redirect_uri: window.location.origin
+        redirect_uri: window.location.origin.includes('aaron') ? 
+          'https://aaron.greider.org/flow-app/dist/index.html' : window.location.origin
       }}
     >
       <App />
