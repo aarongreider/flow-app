4ac064809471a4d028e7b84f15f3ce50eb56fc79
Aaron Greider <aaron.greider@gmail.com>
2025-02-23 17:10:30 -0500

    browserRouter to hashRouter

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    base: mode === 'production' ? 'https://aaron.greider.org/flow-app/dist/' : '/',
+    base: mode === 'production' ? '/flow-app/dist/' : '/',
     plugins: [react()],
   }
 })
