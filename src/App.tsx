eda96cc5701518c217324ee5c068091be7e85855
Aaron Greider <aaron.greider@gmail.com>
2025-02-23 16:11:53 -0500

    production root directory in react router / build

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,21 @@ import { DndContextComp } from './components/DragNDrop/DnDContext';
 
 
 export default function App() {
+  const rootDir = import.meta.env.PROD ? "https://aaron.greider.org/flow-app/dist" : ''
 
   return <>
     <ul style={{ position: 'absolute', left: '50%', zIndex: 200, display: `${window.innerWidth > 450 ? 'block' : 'none'}` }}>
-      <li><Link to="/projects">Projects</Link></li>
-      <li><Link to="/editor">Node Editor</Link></li>
-      <li><Link to="/Dnd">DragNDrop</Link></li>
+      <li><Link to={`${rootDir}/projects`}>Projects</Link></li>
+      <li><Link to={`${rootDir}/editor`}>Node Editor</Link></li>
+      <li><Link to={`${rootDir}/Dnd`}>DragNDrop</Link></li>
     </ul>
     <Firebase />
 
     <Routes >
-      <Route path="/editor/:projectKey/:pageKey" element={<NodeEditor></NodeEditor>} />
-      <Route path="/projects" element={<ProjectHome></ProjectHome>} />
-      <Route path="/Dnd" element={<DndContextComp></DndContextComp>} />
-      <Route path="*" element={<h1>404</h1>} />
+      <Route path={`${rootDir}/editor/:projectKey/:pageKey`} element={<NodeEditor></NodeEditor>} />
+      <Route path={`${rootDir}/projects`} element={<ProjectHome></ProjectHome>} />
+      <Route path={`${rootDir}/Dnd`} element={<DndContextComp></DndContextComp>} />
+      <Route path={`*`} element={<h1>404</h1>} />
     </Routes>
   </>
 }
