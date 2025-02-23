d7539a927ce728aecfb7ca39d48e367edb7b8ebb
Aaron Greider <aaron.greider@gmail.com>
2025-02-23 16:04:52 -0500

    rebuild

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 //#region imports
 import NodeEditor from './components/NodeEditor';
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import { ProjectHome } from './components/Projects Modal/ProjectsPopup';
 import Firebase from './components/Firebase';
 import { DndContextComp } from './components/DragNDrop/DnDContext';
@@ -10,16 +10,16 @@ import { DndContextComp } from './components/DragNDrop/DnDContext';
 export default function App() {
 
   return <>
-    {/* <ul style={{ position: 'absolute', left: '50%', zIndex: 200, display:`${window.innerWidth > 450 ? 'block' : 'none'}` }}>
-      <li><Link to="/">Projects</Link></li>
+    <ul style={{ position: 'absolute', left: '50%', zIndex: 200, display: `${window.innerWidth > 450 ? 'block' : 'none'}` }}>
+      <li><Link to="/projects">Projects</Link></li>
       <li><Link to="/editor">Node Editor</Link></li>
       <li><Link to="/Dnd">DragNDrop</Link></li>
-    </ul> */}
+    </ul>
     <Firebase />
 
     <Routes >
       <Route path="/editor/:projectKey/:pageKey" element={<NodeEditor></NodeEditor>} />
-      <Route path="/" element={<ProjectHome></ProjectHome>} />
+      <Route path="/projects" element={<ProjectHome></ProjectHome>} />
       <Route path="/Dnd" element={<DndContextComp></DndContextComp>} />
       <Route path="*" element={<h1>404</h1>} />
     </Routes>
